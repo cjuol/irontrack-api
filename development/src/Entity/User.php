4f4691e98544c47ff687c477b218aeabb933738d
Cristobal Jurado Oller <c.jurado.oller@gmail.com>
2026-03-10 18:42:00 +0100

    feat(auth): añadir repositorio de usuario

--- development/src/Entity/User.php
+++ development/src/Entity/User.php
@@ -6,6 +6,7 @@ namespace App\Entity;
 
 use App\Entity\Log\TrainingDay;
 use App\Entity\Program\Mesocycle;
+use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -13,7 +14,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Uid\Uuid;
 
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: 'users')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
